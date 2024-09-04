c6d5d7ba6373ab3d86adbac417b8e9ea2f12ca28
giangnam <legiang1199@gmail.com>
2024-09-04 09:17:11 +0700

    anotherFixWithConfig

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,7 @@
-export default () => ({});
+module.exports = {
+  "users-permissions": {
+    config: {
+      jwtSecret: process.env.JWT_SECRET || "your-default-jwt-secret", // Replace with the actual secret or use an environment variable
+    },
+  },
+};
